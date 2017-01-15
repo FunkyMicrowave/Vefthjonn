9a93ea782fc3474458978e0bc9ad3abbb088aef9
FunkyMicrowave <FunkyMicrowave@users.noreply.github.com>
2017-01-15 21:15:08 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body{
+                color: black;
+                background: white;
+            }
+        </style>
+    <title></title>
+</head>
+
+<body>
+    <h1>
+        Halló heimur
+    </h1>
+    
+</body>
+</html>
